aa3e9cf4f267c663823cb1ba179251dbe8453560
MTRNord <mtrnord1@gmail.com>
2018-09-02 17:01:33 +0200

    More debug

--- chatkit/src/main/java/com/stfalcon/chatkit/dialogs/DialogsListAdapter.java
+++ chatkit/src/main/java/com/stfalcon/chatkit/dialogs/DialogsListAdapter.java
@@ -159,7 +159,10 @@ public class DialogsListAdapter<DIALOG extends IDialog>
 
             @Override
             protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                Logger.d(filterResults.count);
+                Logger.d(filterResults.values);
                 itemsListFiltered = (ArrayList<DIALOG>) filterResults.values;
+                Logger.d(itemsListFiltered);
                 notifyDataSetChanged();
             }
         };
