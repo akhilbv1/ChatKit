cbf3fe992be7579dbaf04716a81e71a4dd449a0a
Marcel <MTRNord@users.noreply.github.com>
2018-10-19 22:45:31 +0200

    Try to optimize null Pointer

--- chatkit/src/main/java/com/stfalcon/chatkit/dialogs/DialogsListAdapter.java
+++ chatkit/src/main/java/com/stfalcon/chatkit/dialogs/DialogsListAdapter.java
@@ -384,15 +384,9 @@ public class DialogsListAdapter<DIALOG extends IDialog>
         Collections.sort(items, new Comparator<DIALOG>() {
             @Override
             public int compare(DIALOG o1, DIALOG o2) {
-                if (o1 == null) {
-                    return -1;
-                }
-                if (o2 == null) {
-                    return 1;
-                }
-                if (o1.getLastMessage() == null || o2.getLastMessage() == null) {
-                    return 0;
-                }
+                if (o1 == null && o2 == null) return 0;
+                if (o1 == null) return -1;
+                if (o2 == null) return 1;
                 if (o1.getLastMessage().getCreatedAt().after(o2.getLastMessage().getCreatedAt())) {
                     return -1;
                 } else if (o1.getLastMessage().getCreatedAt().before(o2.getLastMessage().getCreatedAt())) {
